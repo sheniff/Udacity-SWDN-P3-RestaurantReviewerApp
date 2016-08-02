6a44ca6224d79c03bec6399aaebd63cfff9a71d4
Sergio Almecija <sheniff@gmail.com>
2016-08-01 17:24:46 -0700

    Fake review system

--- app/modules/home/detailCtrl.js
+++ app/modules/home/detailCtrl.js
@@ -24,11 +24,34 @@
 	function Detail($state, homeService) {
 		/*jshint validthis: true */
 		var vm = this;
-
 		vm.restaurant = homeService.getRestaurant($state.params.id);
+
 		vm.getNumber = function(num) {
 			return new Array(num);
 		};
+
+		vm.saveReview = function(rating, comment) {
+			vm.restaurant.reviews.splice(0, 0, {
+				restaurantId: vm.restaurant.id,
+				reviewer: 'Snoopy',
+				avatar: 'http://www.emoticonswallpapers.com/avatar/comics/Woodstock--Snoopy.jpg',
+				timestamp: new Date().getTime(),
+				rating: angular.copy(rating),
+				comment: angular.copy(comment)
+			});
+
+			vm.clearForm();
+		};
+
+		vm.clearForm = function() {
+			vm.newReview = {
+				rating: 3,
+				comment: ''
+			};
+		};
+
+		// init
+		vm.clearForm();
 	}
 
 })();
