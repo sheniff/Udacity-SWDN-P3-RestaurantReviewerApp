37ce3e1b8320333adcbc3b3c80b5183a07e0d282
Sergio Almecija <sheniff@gmail.com>
2016-08-02 17:22:16 -0700

    lock focus

--- app/modules/layouts/main-page/mainPageCtrl.js
+++ app/modules/layouts/main-page/mainPageCtrl.js
@@ -24,19 +24,61 @@
 	function Layout($mdSidenav, $cookies, $state, $mdToast, $mdDialog ) {
 		/*jshint validthis: true */
 		var vm = this;
+		var focusedElementBeforeModal;
 
 		vm.toggleSidenav = function (menuId) {
-			$mdSidenav(menuId).toggle();
-		};
-
-		var originatorEv;
-		vm.openMenu = function ($mdOpenMenu, ev) {
-			originatorEv = ev;
-			$mdOpenMenu(ev);
+			var sidenav = $mdSidenav(menuId);
+			sidenav.toggle().then(function() {
+				if (sidenav.isOpen()) {
+					lockFocus();
+				} else {
+					unlockFocus();
+				}
+			});
 		};
 
 		vm.inDashboard = function() {
 			return $state.current.name === 'home.dashboard';
+		};
+
+		var lockFocus = function() {
+			focusedElementBeforeModal = document.activeElement;
+			var modal = document.querySelector('form[name="filterForm"]');
+
+			modal.addEventListener('keydown', trapTabKey);
+
+			var focusableElementsString = 'input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled])';
+			var focusableElements = modal.querySelectorAll(focusableElementsString);
+			focusableElements = Array.prototype.slice.call(focusableElements);
+			var firstTabStop = focusableElements[0];
+			var lastTabStop = focusableElements[focusableElements.length - 1];
+
+			// focus first child
+			firstTabStop.focus();
+
+			// tab trap
+			function trapTabKey(e) {
+				if (e.keyCode === 9) {
+					// shift + tab
+					if (e.shiftKey) {
+						if (document.activeElement === firstTabStop) {
+							e.preventDefault();
+							lastTabStop.focus();
+						}
+
+					// tab
+					} else {
+						if (document.activeElement === lastTabStop) {
+							e.preventDefault();
+							firstTabStop.focus();
+						}
+					}
+				}
+			}
+		};
+
+		var unlockFocus = function() {
+			focusedElementBeforeModal.focus();
 		};
 	}
 
